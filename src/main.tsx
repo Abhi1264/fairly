3a658442430ba6c1d66bcc4250cf9d14ed5f19c7
Abhinav Kumar Choudhary <abhinav.kumar.1264@gmail.com>
2025-06-22 17:50:21 +0530

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { inject } from '@vercel/analytics';
 import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./lib/store";
+
+inject();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
